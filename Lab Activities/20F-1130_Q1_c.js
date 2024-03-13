4f7ab1168bbba64ced1588bf4d526f9ebdce54c5
Zain Mehmood <108570792+ZainMehmood123@users.noreply.github.com>
2024-03-13 22:54:55 +0500

    add
    add

---
+++ Lab Activities/20F-1130_Q1_c.js
@@ -0,0 +1,10 @@
+// In JavaScript, you can use
+// for single-line comments or /* */ for multi-line comments.
+// This is a JavaScript comment
+var x = 5;
+
+/*
+This is a multi-line comment.
+It spans multiple lines.
+*/
+var y = 10;
